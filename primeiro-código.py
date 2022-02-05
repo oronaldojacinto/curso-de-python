fb009b7f6c8542368c6ad0bb7be644f0a8686655
Ronaldo Jacinto <scalleinfo@gmail.com>
2022-02-05 14:33:31 -0300

    Initial Commit

---
+++ primeiro-código.py
@@ -0,0 +1,70 @@
+'''
+# Meu primeiro Código
+print("Olá!")
+print('Tudo bem?')
+print('Que dia é hoje?')
+'''
+
+# Tipos de Dados # Data Types #
+# Text = String ( str )
+# Numbers = Integer = inteiro (1,2,3...)
+# Float = Fração (3.5, 4.7, 10.6 etc...)
+# Boolean Type = Bool: (true ou False)
+# Variables/Variáveis => exemplo 2 = olá ou x = 2 por exemplo ou y = olá
+
+# x = 2
+# print(x * x + x)
+
+# x = str(3)
+# y = int(4)
+# z = float(5)
+
+# print(x + x)
+# print(y + y)
+# print(z + z)
+
+# O André tem 32 anos de idade e mora na cidade de São Paulo #
+
+# nome = 'Ándré'
+# idade = 32
+# idade = str(idade)
+# cidade = 'São Paulo'
+
+# print('O ' + nome + ' tem ' + str(idade) + ' anos de idade e mora em ' + cidade + '.' )
+
+# nome = input('Qual é o seu nome: ')
+# idade = input('Qual é a sua idade: ')
+# idade = str(idade)
+# cidade = input('Onde você mora: ')
+
+# print('O ' + nome + ' tem ' + str(idade) + ' anos de idade e mora em ' + cidade + '.' )
+
+# ano_nascimento = input('Em que ano você nasceu? ')
+# print(type(ano_nascimento))
+# idade = 2020 - int(ano_nascimento)
+# print(idade)
+
+#fruta = 'Abacate'
+#index 0123456 - Slice
+#print(fruta[-6])
+
+#valor = 99.75
+#valor = str(valor)
+#print(valor[3:5])
+
+# O Marcos Silva e um excelente [Programador]
+
+# nome = 'Marcos'
+# sobrenome = 'Silva'
+# profissao = 'Programador'
+
+# texto = 'O ' + nome + ' ' + sobrenome + ' e um excelente ' + ' [' + profissao + ']'
+# print(texto)
+
+# texto2 = f'O{nome} {sobrenome} e um excelente [{profissao}]'
+
+# Métodos para Strings
+
+# mensagem = 'Eu adoro comida Caseira'
+#index.     0123456789
+#print(mensagem.find('C'))
